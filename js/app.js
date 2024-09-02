142c6e30b1b259228d6b33f959481013fe439e7b
Fauzan Rosyadi <fauzan.rosyadi@gmail.com>
2024-09-02 15:00:37 +0700

    add check for winner functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+const mutationConfig = { 
+    attributes: true,
+    childList: true,
+    subtree: true 
+};
+
 let currentPlayer = 'X';
 let gameStatusMsg = 'Player X Turn';
 let winner = undefined;
@@ -10,7 +16,7 @@ if (!window.localStorage.getItem('boardScore')) {
     boardScore = JSON.parse(window.localStorage.getItem('boardScore'));
 }
 
-
+const board = document.querySelector('.board');
 const squares = document.querySelectorAll('.square');
 const newGameBtn = document.querySelector('#newgame');
 const gameStatus = document.querySelector('#game-status');
@@ -25,6 +31,7 @@ newGameBtn.addEventListener('click', () => {
         gameStatusMsg = 'Player X Turn';
         boardScore = Array(9).fill(null);
         window.localStorage.setItem('boardScore', JSON.stringify(boardScore));
+        winner = undefined;
     })
 })
 
@@ -51,7 +58,7 @@ function getSquareClass(cp) {
 }
 
 function checkWinner() {
-    const lines = [
+    const winningLocs = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -62,13 +69,18 @@ function checkWinner() {
         [2, 4, 6],
       ];
 
-    winningLocs.forEach(loc => {
-        console.log(loc)
-    })
+    for (const loc of winningLocs) {
+        const [a, b, c] = loc;
+        if (boardScore[a] && boardScore[a] === boardScore[b] &&  boardScore[a] === boardScore[c]) {
+            winner = boardScore[a]
+            gameStatus.textContent = `Winner is ${winner}`;
+            break;
+        }
+    }
 }
 
 function clickFillSquare(event) {
-    if (!event.target.textContent) {
+    if (!event.target.textContent && !winner) {
         event.target.textContent = currentPlayer;
         event.target.classList.add(getSquareClass(currentPlayer));
         const idx = parseInt(event.target.dataset.location);
@@ -77,6 +89,7 @@ function clickFillSquare(event) {
         switchPlayer();
     }
 }
+
 squares.forEach(s => {
     s.addEventListener('click', clickFillSquare);
     const idx = parseInt(s.dataset.location);
@@ -85,4 +98,10 @@ squares.forEach(s => {
         s.textContent = locValue;
         s.classList.add(getSquareClass(locValue));
     }
+});
+
+let observer = new MutationObserver(records => {
+    checkWinner();
 })
+
+observer.observe(board, mutationConfig)
